f40bf5032d381411e1f713a2843f903552b07b0b
wuchu <lanmit919731@163.com>
2024-01-01 16:26:34 +0800

    [eureka 模拟第三方指数数据采集服务-4, 配置redis缓存指数码]

--- index-gather-store-service/src/main/java/my/chu/service/IndexService.java
+++ index-gather-store-service/src/main/java/my/chu/service/IndexService.java
@@ -1,9 +1,13 @@
 package my.chu.service;
 
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.collection.CollectionUtil;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import my.chu.pojo.Index;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,17 +16,45 @@ import java.util.List;
 import java.util.Map;
 
 @Service
+@CacheConfig(cacheNames="indexes")
 public class IndexService {
     @Autowired
     RestTemplate restTemplate;
 
-    private List<Index> indices;
-    @HystrixCommand(fallbackMethod = "third_part_service_timeout")
+    /**
+    * 自我注入形式解决直接在类内部调用方法
+    * （如 remove 和 store）不会触发 Spring 的代理相关功能，
+    * 例如缓存相关的注解（@Cacheable、@CacheEvict）
+    **/
+    @Autowired
+    private IndexService self; //
+
+    private List<Index> indexes;
+
     public List<Index> fetchIndexData() {
 
         List<Map> mapList = restTemplate.getForObject("http://127.0.0.1:8090/indexes/codes.json", List.class);
 
         return map2Index(mapList);
+    }
+    @HystrixCommand(fallbackMethod = "third_part_service_timeout")
+    public List<Index> fresh() {
+        indexes =fetchIndexData(); // 获取指数数据到成员变量中
+        self.remove();
+        return self.store();
+    }
+    @CacheEvict(allEntries=true)
+    public void remove(){
+    }
+
+    @Cacheable(key="'all_codes'")
+    public List<Index> get(){
+        return CollUtil.toList();
+    }
+
+    @Cacheable(key="'all_codes'")
+    public List<Index> store(){
+        return indexes;
     }
 
     public List<Index> third_part_service_timeout() {
@@ -35,7 +67,7 @@ public class IndexService {
     }
 
     private List<Index> map2Index(List<Map> mapList) {
-        List<Index> indices = new ArrayList<>();
+        List<Index> indexes = new ArrayList<>();
         // todo : 后续改为其他调用方法进行转换
         for(Map map: mapList) {
             String code = map.get("code").toString();
@@ -43,8 +75,8 @@ public class IndexService {
             Index index= new Index();
             index.setCode(code);
             index.setName(name);
-            indices.add(index);
+            indexes.add(index);
         }
-        return indices;
+        return indexes;
     }
 }
